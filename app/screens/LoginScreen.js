3d91d73f1c783ea34da9e7719f2d514608d718e1
adambmc <adam.bmc@gmail.com>
2016-07-18 17:33:45 +0300

    10

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -51,7 +51,7 @@ const styles = StyleSheet.create({
       
       
  _ClearCredentials(){
-        console.log("Login9")
+        console.log("Login10")
         AsyncStorage.multiRemove(["kenestoU","kenestoP", "env"]); 
     }
    
