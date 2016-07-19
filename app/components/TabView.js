d7a0bb775b3a1805f442ed50fe81d93461928dac
glutik <glutik@gmail.com>
2016-07-19 16:22:30 +0300

    7

--- app/components/TabView.js
+++ app/components/TabView.js
@@ -178,7 +178,7 @@ class TabView extends React.Component {
 
 
     SelectItem(menuitem : Object){
-        console.log('666')
+        //console.log('666')
         menuitem.selected = true;
         alert(menuitem.Id)
         console.log('444')
